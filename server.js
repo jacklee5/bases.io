78c15100f22e9bd6682e645f8adcc15efee3a50e
Jack Lee <jack200336@gmail.com>
2018-06-24 18:10:47 -0400

    the last of p2

--- server.js
+++ server.js
@@ -46,11 +46,19 @@ const MAP_SIZE = 1000;
 //the size of the player
 const PLAYER_SIZE = 25;
 //the movement speed of the players
-const MOVEMENT_SPEED = 4;
+const MOVEMENT_SPEED = 240;
 //base size
 const BASE_SIZE = 200;
 //starting hp for base
 const BASE_HP = 1000;
+//collision groups
+const COLLISION_GROUPS = {
+    PLAYER: Math.pow(2, 0),
+    OBJECT: Math.pow(2, 1),
+    ITEM: Math.pow(2, 2),
+    NONE: 0,
+    ALL: -1
+}
 
 /** hopefully helpful helpers **/
 //get a ramdom alphanumeric ID
@@ -97,6 +105,63 @@ io.on("connection", (socket) => {
             }
         }
         
+        //Create invisible walls around the world      
+        let leftWallBody = new p2.Body({
+            type: p2.Body.STATIC,
+            position: [-5, WAITING_MAP_SIZE / 2],
+            id: "WALLLEFT"
+        });
+        let leftWallShape = new p2.Box({
+            width: 10,
+            height: WAITING_MAP_SIZE,
+            collisionGroup: COLLISION_GROUPS.OBJECT,
+            collisionMask: COLLISION_GROUPS.ALL
+        });
+        leftWallBody.addShape(leftWallShape);
+        games[roomId].physics.addBody(leftWallBody);
+
+        let rightWallBody = new p2.Body({
+            type: p2.Body.STATIC,
+            position: [WAITING_MAP_SIZE + 5, WAITING_MAP_SIZE / 2],
+            id: "WALLRIGHT"
+        });
+        let rightWallShape = new p2.Box({
+            width: 10,
+            height: WAITING_MAP_SIZE,
+            collisionGroup: COLLISION_GROUPS.OBJECT,
+            collisionMask: COLLISION_GROUPS.ALL
+        });
+        rightWallBody.addShape(rightWallShape);
+        games[roomId].physics.addBody(rightWallBody);
+
+        let topWallBody = new p2.Body({
+            type: p2.Body.STATIC,
+            position: [WAITING_MAP_SIZE / 2, -5],
+            id: "WALLTOP"
+        });
+        let topWallShape = new p2.Box({
+            width: WAITING_MAP_SIZE,
+            height: 10,
+            collisionGroup: COLLISION_GROUPS.OBJECT,
+            collisionMask: COLLISION_GROUPS.ALL
+        });
+        topWallBody.addShape(topWallShape);
+        games[roomId].physics.addBody(topWallBody);
+
+        let bottomWallBody = new p2.Body({
+            type: p2.Body.STATIC,
+            position: [WAITING_MAP_SIZE / 2, WAITING_MAP_SIZE + 5],
+            id: "WALLBOTTOM"
+        });
+        let bottomWallShape = new p2.Box({
+            width: WAITING_MAP_SIZE,
+            height: 10,
+            collisionGroup: COLLISION_GROUPS.OBJECT,
+            collisionMask: COLLISION_GROUPS.ALL
+        });
+        bottomWallBody.addShape(bottomWallShape);
+        games[roomId].physics.addBody(bottomWallBody);
+        
         //add player to room
         playerRooms[socket.id] = roomId;
         socket.join(roomId);
@@ -114,8 +179,8 @@ io.on("connection", (socket) => {
             name: name,
             team: team,
             pos: {
-                x: Math.floor(Math.random() * WAITING_MAP_SIZE),
-                y: Math.floor(Math.random() * WAITING_MAP_SIZE),
+                x: Math.floor(Math.random() * (WAITING_MAP_SIZE - PLAYER_SIZE * 2)) + PLAYER_SIZE,
+                y: Math.floor(Math.random() * (WAITING_MAP_SIZE - PLAYER_SIZE * 2)) + PLAYER_SIZE,
                 rot:0,
                 rightHand: {
                     x: 0,
@@ -134,7 +199,11 @@ io.on("connection", (socket) => {
             position: [games[roomId].players[socket.id].pos.x, games[roomId].players[socket.id].pos.y],
             id: `player${socket.id}`
         });
-        playerBody.addShape(new p2.Circle({radius: PLAYER_SIZE}));
+        playerBody.addShape(new p2.Circle({
+            radius: PLAYER_SIZE,
+            collisionGroup: COLLISION_GROUPS.PLAYER,
+            collisionMask: COLLISION_GROUPS.OBJECT
+        }));
         games[roomId].physics.addBody(playerBody);
 
         console.log(`${name} joined room ${roomId} in team ${TEAM_NAMES[team]}`);
@@ -176,10 +245,11 @@ setInterval(() => {
                     games[i].state = STATES.PLAYING;
                     io.in(i).emit("state change", STATES.PLAYING);
                     games[i].map.size = MAP_SIZE;
+                    games[i].physics = new p2.World({});
                     for(let j in games[i].players){
                         //morph into playing state
-                        games[i].players[j].pos.x = Math.floor(Math.random() * MAP_SIZE);
-                        games[i].players[j].pos.y = Math.floor(Math.random() * MAP_SIZE);
+                        games[i].players[j].pos.x = Math.floor(Math.random() * (MAP_SIZE - PLAYER_SIZE * 2)) + PLAYER_SIZE;
+                        games[i].players[j].pos.y = Math.floor(Math.random() * (MAP_SIZE - PLAYER_SIZE * 2)) + PLAYER_SIZE;
                         games[i].physics.getBodyById(`player${j}`).position = [games[i].players[j].pos.x, games[i].players[j].pos.y]
                         games[i].bases[TEAMS.RED] = {
                             pos: {
@@ -190,6 +260,18 @@ setInterval(() => {
                             hp: BASE_HP,
                             team: TEAMS.RED
                         }
+                        let baseBody = new p2.Body({
+                            mass: 0,
+                            position: [MAP_SIZE / 2, BASE_SIZE / 2 + PLAYER_SIZE * 2],
+                            id: "basered"
+                        });
+                        baseBody.addShape(new p2.Box({
+                            width: BASE_SIZE,
+                            height: BASE_SIZE,
+                            collisionGroup: COLLISION_GROUPS.OBJECT,
+                            collisionMask: COLLISION_GROUPS.PLAYER
+                        }));
+                        games[i].physics.addBody(baseBody);
                         games[i].bases[TEAMS.GREEN] = {
                             pos: {
                                 x: MAP_SIZE - BASE_SIZE / 2 - PLAYER_SIZE * 2,
@@ -240,7 +322,7 @@ setInterval(() => {
                 }
                 games[i].physics.getBodyById(`player${j}`).velocity = [vx, vy];
             }  
-            games[i].physics.step(1);
+            games[i].physics.step(1 / 60);
             for(let j in games[i].players){
                 let pos = games[i].physics.getBodyById(`player${j}`).position;
                 games[i].players[j].pos.x = pos[0];
