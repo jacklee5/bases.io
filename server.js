1fc4315ecf6a1c6c46ead907a6743aafab2a0e66
Jack Lee <jack200336@gmail.com>
2018-06-24 13:12:58 -0400

    fix port

--- server.js
+++ server.js
@@ -14,7 +14,8 @@ app.get('/', function(request, response) {
   response.sendFile(path.join(__dirname, 'index.html'));
 });
 // Starts the server.
-server.listen(5000, function() {
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, function() {
   console.log('Starting server on port 5000');
 });
 
